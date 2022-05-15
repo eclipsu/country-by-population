531b92f6004582ddbab5b805cc13355b992739c9
unknown <stharj219@gmail.com>
2022-05-16 01:19:15 +0545

    Two countries comming up - Solved by recursive method

--- Server/controlers/game.js
+++ Server/controlers/game.js
@@ -1,9 +1,14 @@
 const metadata = require("../utils/metadata.json");
 
-const game = async (req, res) => {
+const getCountries = () => {
   var country_1 = metadata[Math.floor(Math.random() * metadata.length)];
   var country_2 = metadata[Math.floor(Math.random() * metadata.length)];
-  await res.status(200).json({ country_1: { name: country_1.name, population: country_1.population }, country_2: { name: country_2.name, population: country_2.population } });
+  if (country_1.name === country_2.name) return getCountries();
+  return { country_1: { name: country_1.name, population: country_1.population }, country_2: { name: country_2.name, population: country_2.population } };
+};
+
+const game = async (req, res) => {
+  return await res.status(200).json(getCountries());
 };
 
 module.exports = { game };
